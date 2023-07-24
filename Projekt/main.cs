444ee266181981b1bfd5a8eb294dbef9e88bf31a
arvid404 <h21arsod@du.se>
2023-07-24 23:25:51 +0200

    lagt till rader
    dick

--- Projekt/main.cs
+++ Projekt/main.cs
@@ -7,6 +7,10 @@ namespace HelloWorld
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");    
     }
   }
 }
